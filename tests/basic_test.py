5a9562b2210e8f5eff6bd305c654d3b04305d0a9
Connor Stone <connorstone628@gmail.com>
2021-11-13 15:30:46 -0500

    updating code structure. Better Pipe object design to fit in flowcharts. Chart now iterable.

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -24,16 +24,16 @@ def dec1(state):
     return state['choose']
 
 def proc4(state):
-    print('final process. current state: ', state)
+    print('in process 4. current state: ', state)
     return state
 
 
 # Test build chart construction, explicit
 ######################################################################
 print('test 1')
-methods = [flow.process('P1', proc1), flow.process('P2', proc2), flow.process('P3', proc3), flow.decision('D1', dec1), flow.process('P4', proc4)]
+methods = [flow.Process('P1', proc1), flow.Process('P2', proc2), flow.Process('P3', proc3), flow.Decision('D1', dec1), flow.Process('P4', proc4)]
 
-C1 = flow.chart('C1')
+C1 = flow.Chart('C1')
 C1.build_chart(methods, {'start': 'P1', 'P1':'P2', 'P2': 'P3', 'P3': 'D1', 'D1':['P2','P4'], 'P4': 'end'})
 print(C1)
 C1.draw('basic_test.png')
@@ -42,7 +42,7 @@ CC1 = deepcopy(C1)
 
 res = CC1({'first': 'first message'})
 print(res)
-
+print('benchmarks: ', CC1.benchmark, CC1.benchmarks, sum(CC1.benchmarks))
 # Test nesting charts
 ######################################################################
 print('test 2')
@@ -51,9 +51,9 @@ def newbeginning(state):
     state.update({'first': 'different first message'})
     return state
 
-C2 = flow.chart('C2')
+C2 = flow.Chart('C2')
 
-C2.add_node(flow.process('Pnew', newbeginning))
+C2.add_node(flow.Process('Pnew', newbeginning))
 C2.add_node(C1)
 C2.link_nodes('Pnew', 'C1')
 C2.link_nodes('start', 'Pnew')
@@ -62,12 +62,14 @@ C2.link_nodes('C1', 'end')
 C2.draw('basic_test_meta.png')
 
 print(C2({'first': 'this message will be erased'}))
+print('benchmarks: ', C2.benchmark, C2.benchmarks, sum(C2.benchmarks))
+
 
 # test linear mode linking
 ######################################################################
 print('test 3')
 
-C3 = flow.chart('C3')
+C3 = flow.Chart('C3')
 C3.linear_mode(True)
 C3.add_process_node('P1', proc1)
 C3.add_process_node('P2', proc2)
@@ -80,3 +82,4 @@ C3.link_nodes('D1', 'P2')
 C3.draw('basic_test_linear.png')
 
 print(C3({'first': 'first message from linear mode chart'}))
+print('benchmarks: ', C3.benchmark, C3.benchmarks, sum(C3.benchmarks))
